c0a8f01ec295d4884420de7950bd81e7422572bb
axelsccp <apaschoabarres@gmail.com>
2020-07-20 23:10:50 -0300

    modifica database para função

---
+++ src/database/database.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "admin"
+	password = "admin"
+	dbname   = "tcd_amazon"
+)
+
+func Connection() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+
+	fmt.Println("Successfully connected!")
+
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
---
+++ src/database/go.mod
@@ -0,0 +1,5 @@
+module database
+
+go 1.14
+
+require github.com/lib/pq v1.7.1 // indirect
